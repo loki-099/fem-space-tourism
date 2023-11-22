aec2c1ffe8789b77153e1436ca9ae52f0a42fa3b
loki <loidatolentino358@gmail.com>
2023-11-22 22:49:48 +0800

    configure tailwind file path

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,9 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'home-mobile': "url('./assets/home/background-home-mobile.jpg')",
-        'home-tablet': "url('./assets/home/background-home-tablet.jpg')",
-        'home-desktop': "url('./assets/home/background-home-desktop.jpg')"
+        'home-mobile': "url('/assets/home/background-home-mobile.jpg')",
+        'home-tablet': "url('/assets/home/background-home-tablet.jpg')",
+        'home-desktop': "url('/assets/home/background-home-desktop.jpg')"
       }
     }
   },
