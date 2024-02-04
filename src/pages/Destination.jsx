567a9847345eafc17a2472e6288963100e944a17
mischiefluis <loidatolentino358@gmail.com>
2024-02-04 22:00:40 +0800

    information.json

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -5,7 +5,9 @@ const Destination = () => {
   return (
     <div className='w-full h-screen overflow-y-auto bg-destination-mobile bg-no-repeat bg-cover md:bg-destination-tablet xl:bg-destination-desktop'>
       <Header/>
-      <h1 className='text-black'>Idol</h1>
+      <div>
+        
+      </div>
     </div>
   )
 }
