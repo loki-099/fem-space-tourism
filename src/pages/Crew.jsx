213652bc9954d07c6f0b0129d78f470b110387ba
mischiefluis <loidatolentino358@gmail.com>
2024-02-09 19:52:27 +0800

    tablet crew

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -27,15 +27,15 @@ const Crew = () => {
   return (
     <div className='relative w-full h-screen overflow-y-auto bg-crew-mobile bg-no-repeat bg-cover md:bg-crew-tablet xl:bg-crew-desktop'>
       <Header/>
-      <div className='px-6 md:px-9 xl:px-[136px] xl:pb-14 my-6 md:my-10 flex flex-col items-center md:items-baseline'>
+      <div className='px-6 md:px-9 xl:px-[136px] xl:pb-14 my-6 md:my-0 flex flex-col items-center md:items-baseline md:pb-0'>
         <h5 className='heading-5 uppercase text-accent'><span className='font-bold opacity-25 mr-3'>02</span>Meet your Crew</h5>
-        <div className='mt-8 w-full'>
+        <div className='mt-8 md:mt-16 w-full md:flex md:flex-col-reverse'>
           {/* SWIPER */}
-          <div className='w-full mx-auto max-w-[500px] h-[225px]'>
+          <div className='w-full mx-auto max-w-[500px] md:max-w-[900px] h-[225px] md:h-[532px] md:mt-10'>
             <CrewSwiper crews={crews} slide={swiperRef} setCurCrew={setCurCrew}/>
           </div>
           {/* NAME, POSITION, INFO, BUTTON */}
-          <div className='mt-8 flex flex-col gap-8'>
+          <div className='mt-8 flex flex-col gap-8 md:gap-10 md:flex-col-reverse md:mt-0'>
             <div className='flex justify-center gap-4 '>
               {crews.map((crew, index) => (
                 <button 
@@ -48,7 +48,7 @@ const Crew = () => {
             <div className='flex flex-col items-center'>
               <h4 className='heading-4 uppercase opacity-50'>{cur.position}</h4>
               <h3 className='heading-3 uppercase'>{cur.name}</h3>
-              <p className='mt-4 body text-center pb-8'>{cur.body}</p>
+              <p className='mt-4 body text-center pb-8 md:pb-0 md:w-[80%]'>{cur.body}</p>
             </div>
           </div>
         </div>
